8d74a8c9e0c52c1b42cfd186a73ba4e51fbd2612
ITFrister <it.frister@gmail.com>
2025-04-18 14:58:21 +0500

    activity lifecycle

--- app/src/main/java/com/example/lesson/MainActivity.kt
+++ app/src/main/java/com/example/lesson/MainActivity.kt
@@ -2,15 +2,19 @@ package com.example.lesson
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lesson.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val sleepTime: Long = 0
+    private var newPassword: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,5 +45,56 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+
+        Thread.sleep(sleepTime)
+
+        binding.root.setOnClickListener {
+            newPassword = binding.newPasswordEdit.text.toString()
+        }
+
+        Log.d("ACTIVITY_LIFECYCLE", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Thread.sleep(sleepTime)
+
+
+        Log.d("ACTIVITY_LIFECYCLE", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Thread.sleep(sleepTime)
+
+        Log.d("ACTIVITY_LIFECYCLE", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Thread.sleep(sleepTime)
+
+        Log.d("ACTIVITY_LIFECYCLE", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Thread.sleep(sleepTime)
+
+        Log.d("ACTIVITY_LIFECYCLE", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Thread.sleep(sleepTime)
+
+        Log.d("ACTIVITY_LIFECYCLE", newPassword)
+
+        Log.d("ACTIVITY_LIFECYCLE", "onDestroy")
     }
 }
